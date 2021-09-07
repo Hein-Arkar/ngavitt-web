64f59146aa0588db2e1dad071fb9918761f4f7f7
Hein Arkar <arkar131429@gmail.com>
2021-09-07 13:44:24 +0630

    fix scrollbar bug

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "a69b73b525b3fd34439a6468f62db8bf",
-"/": "a69b73b525b3fd34439a6468f62db8bf",
+  "index.html": "7b47f83c2a7a492a9500d6db69ad810d",
+"/": "7b47f83c2a7a492a9500d6db69ad810d",
 "version.json": "36c9b81601858183cd66f770fd9a6b72",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"main.dart.js": "977172cf41defdbf71e21f38da05843c",
+"main.dart.js": "32586a686eb7f2f7a1f4f3b95e1a9bc5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "7aaf3996738086bbd796613e14ef9e45",
